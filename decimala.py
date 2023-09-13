26cf9686640edc1c5a9f9f29bd7dcb6654858b30
Joshua Jackson <masterjack612@gmail.com>
2023-09-13 11:52:36 +0100

    maths modules

---
+++ decimala.py
@@ -0,0 +1,9 @@
+from decimal import Decimal
+
+sum = Decimal(0.1)
+sum += Decimal(0.1)
+sum += Decimal(0.1)
+sum -= Decimal(0.3)
+
+print(sum)
+
---
+++ maths.py
@@ -0,0 +1,72 @@
+import math
+
+# Whenever you import a module, you will have access to all the methods in it
+# Now, to have access to all those methods, You have to reference the module for each of the methods you are calling
+
+print("Ceil(4.4) = ", math.ceil(4.4))
+
+print("floor(4.4) = ", math.floor(4.4))
+
+print("fabs(-4.4) = ", math.fabs(-4.4))
+
+# Factorial is calculated as 1 x 2 x 3 x 4 x ... x
+
+print("Factorial(4) = ", math.factorial(4))
+
+
+# To return remainder of a division
+print("fmod(5, 4) = ", math.fmod(5, 4))
+
+
+
+# To receive a float and return an int
+print("trunc(4.3) = ", math.trunc(4.3))
+print("trunc(4.8) = ", math.trunc(4.8))
+
+
+
+# Return x^y
+print("3^2 is written as pow(3, 2) = ", math.pow(3, 2))
+
+# Special Values in python are expressed below
+
+print("math.e = ", math.e)
+
+print("Value of pi = ", math.pi)
+
+# Return e^x
+
+print("exp(4) = ", math.exp(4))
+
+# The log of a number is the power of a number when it is expressed in a certain base
+
+
+
+# Return the natural Logarithm e * e * e ~= 20
+# So log(20) tells you that e^3 ~= 20
+
+print("log(20) = ", math.log(20))
+
+print("log(1000, 10) = ", math.log(1000, 10))
+
+# You can also use base 10 like this
+
+print("log10(1000) = ", math.log10(1000))
+
+print("log2(8) = ", math.log2(8))
+
+
+# We have all the trigonometric functions for each trig ratio
+
+# sin, cos, tan, asin, acos, atan, atan2, asinh, acosh, cosh, tanh, sinh, etc
+
+# math.cos2(30),  (math.cos(30))**2
+
+# Convert radians to degrees and vice verse
+
+print("degrees(1.5708) = ", math.degrees(1.5708))
+
+print("radians(90) = ", math.radians(90))
+
+
+
